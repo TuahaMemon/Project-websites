703884f3ce143ae51d072b16c389f1df60e43ea6
Tuaha memon <tuaha.me@gmail.com>
2022-07-11 11:06:58 -0700

    javascript'hello world'

---
+++ variable.js
@@ -0,0 +1,18 @@
+let name =("tuaha")
+        console.log("name:",name);
+
+        let english =87;
+        let math = 95;
+        let urdu = 75;
+        let scince = 63;
+        let physics = 47;
+        let chemistry = 98;
+        let biology = 76;
+        let computer = 81;
+
+        let totalnumbers = english + urdu + math + scince + chemistry + physics + biology + computer;
+        console.log("totalnumbers:",totalnumbers);
+        // let enur =english + urdu
+        // let coma =computer + math
+        // let totalmarks =coma + enur
+        // console.log("totalmarks:",totalmarks)
